fd29435ebc6a878c52a6a2b2b56eea666e9ec0ce
Chirag H M <chiraghm2004@gmail.com>
2024-09-13 20:06:21 +0530

    initial commit

---
+++ frontend/src/landing_page/OpenAccount.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function OpenAccount() {
+    return ( 
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+                <h1 className='mt-5 pt-5'>Open a Zerodha account</h1>
+                <p className='fs-5 py-2 my-2 text-muted'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+                <button className='p-2 mt-2 btn btn-primary text-white fs-5 ' style={{width:"15%",margin:"0 auto"}}>Sign up now</button> 
+            </div>
+        </div>
+     );
+}
+
+export default OpenAccount;
