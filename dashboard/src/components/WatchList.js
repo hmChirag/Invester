590868e625367e7283e332da93f6544d9e2392fa
Chirag H M <chiraghm2004@gmail.com>
2024-09-15 01:33:33 +0530

    added the watch list to dashboard

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import {Tooltip,Grow} from "@mui/material";
+import { watchlist } from "../data/data";
+import { BarChartOutlined, KeyboardArrowDown, KeyboardArrowUp, MoreHoriz } from "@mui/icons-material";
+
 
 const WatchList = () => {
   return (
@@ -11,12 +15,69 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts">{watchlist.length}</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock,index)=>{
+          return <WatchListItem stock={stock} key={index}/>
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+
+const WatchListItem =({stock})=>{
+  const [showWatchlistActions,setShowWatchlistActions]=useState(false);
+
+  const handleMouseEnter = (e)=>{
+    setShowWatchlistActions(true);
+  }
+
+  const handleMouseLeave =(e)=>{
+    setShowWatchlistActions(false);
+  }
+
+  return(
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? 
+            (<KeyboardArrowDown className="down"/>)
+            : (<KeyboardArrowUp className="up"/>)
+          }
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchlistActions && <WatchListActions uid={stock.name}/>}
+    </li>
+  )
+}
+
+const WatchListActions = ({uid})=>{
+  return <span className="actions">
+    <span>
+      <Tooltip title="Buy (B)" placeholder="top">
+        <button className="buy">Buy</button>
+      </Tooltip>
+      <Tooltip title="Sell (S)" placeholder="top">
+        <button className="sell">Sell</button>
+      </Tooltip>
+      <Tooltip title="Analytics (A)" placeholder="top">
+       <button className="action"> 
+         <BarChartOutlined className="icon"/>
+         </button>
+      </Tooltip>
+      <Tooltip title="More" placeholder="top">
+      <button className="action"> 
+         <MoreHoriz className="icon"/>
+         </button>
+      </Tooltip>
+    </span>
+  </span>
+} 
